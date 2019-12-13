770f41cc7f6ac9eab078d907295ab0eeb7ba28f0
Ukaegbu <ukaegbu@lancaster.ac.uk>
2019-12-13 13:14:06 +0000

    Removed handling of BluetoothDeviceState.disconnected event due to instability

---
+++ lib/src/Commands.dart
@@ -0,0 +1,17 @@
+// Nuki Bluetooth API Commands
+const int REQUEST_DATA_CMD = 0x01;  
+const int PUBLIC_KEY_CMD = 0x03;
+const int CHALLENGE_CMD = 0x04;
+const int AUTHORIZATION_AUTHENTICATOR_CMD = 0x05;
+const int AUTHORIZATION_DATA_CMD = 0x06;
+const int AUTHORIZATION_ID_CMD = 0x07;
+const int AUTHORIZATION_DATA_INVITE_CMD = 0x0B;
+const int KEYTURNER_STATE_CMD = 0x0C;
+const int LOCK_ACTION_CMD = 0x0D;
+const int STATUS_CMD = 0x0E;
+const int AUTHORIZATION_ID_CONFIRMATION_CMD = 0x1E;
+const int AUTHORIZATION_ID_INVITE_CMD = 0x1F;
+const int ERROR_REPORT_CMD = 0x12;
+const int GET_CONFIG_CMD = 0x14;
+const int CONFIG_CMD = 0x15;
+const int SET_PIN_CMD = 0x19;
